bf52298e79116f4f84067a87e96126e350f0300c
danjiewu <danjiewu@hotmail.com>
2026-04-11 22:42:02 +0800

    Bug fix: VendorSpecificAttribute supports multiple vendor attributes

--- Flexinets.Radius.Core/src/Dictionary/VendorSpecificAttribute.cs
+++ Flexinets.Radius.Core/src/Dictionary/VendorSpecificAttribute.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace Flexinets.Radius.Core
 {
     public class VendorSpecificAttribute
     {
-        public readonly byte Length;
-        public readonly uint VendorId;
-        public readonly byte VendorCode;
-        public readonly byte[] Value;
+        public uint VendorId;
 
+        public List<VendorAttrNode> AttrNodes { get; } = new List<VendorAttrNode>();
 
         /// <summary>
         /// Create a vsa from bytes
@@ -17,10 +15,43 @@ namespace Flexinets.Radius.Core
         /// <param name="contentBytes"></param>
         public VendorSpecificAttribute(byte[] contentBytes)
         {
-            VendorId = BitConverter.ToUInt32(contentBytes[..4].Reverse().ToArray());
-            VendorCode = contentBytes[4];
-            Length = contentBytes[5];
-            Value = contentBytes[6..];
+            var vendorId = new byte[4];
+            Buffer.BlockCopy(contentBytes, 0, vendorId, 0, 4);
+            Array.Reverse(vendorId);
+            VendorId = BitConverter.ToUInt32(vendorId, 0);
+
+            // VendorSpecificAttribute supports multiple vendor attributes, each vendor attribute has the format of:
+            // 1 byte for VendorCode
+            // 1 byte for Length
+            // (Length - 2) bytes for Value
+            int offset = 4;
+            while (offset < contentBytes.Length)
+            {
+                VendorAttrNode node = new VendorAttrNode();
+                var vendorType = new byte[1];
+                Buffer.BlockCopy(contentBytes, offset, vendorType, 0, 1);
+                node.VendorCode = vendorType[0];
+                offset++;
+
+                var vendorLength = new byte[1];
+                Buffer.BlockCopy(contentBytes, offset, vendorLength, 0, 1);
+                node.Length = vendorLength[0];
+                offset++;
+
+                var value = new byte[node.Length - 2];
+                Buffer.BlockCopy(contentBytes, offset, value, 0, node.Length - 2);
+                node.Value = value;
+                offset += node.Length - 2;
+
+                AttrNodes.Add(node);
+            }
+        }
+
+        public class VendorAttrNode
+        {
+            public byte Length;
+            public byte VendorCode;
+            public byte[]? Value;
         }
     }
 }
